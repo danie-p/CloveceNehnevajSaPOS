2f0ab132e97d80d64858387237fa57ab6015c139
Kristián Lepka <kristianl12@proton.me>
2024-01-03 15:11:17 +0100

    client files update

--- Client/MySocket.cpp
+++ Client/MySocket.cpp
@@ -1,18 +1,26 @@
 #include "MySocket.h"
-#include <iostream>
+#include <stdexcept>
+#include <ws2tcpip.h>
+#include <winsock2.h>
 
 #define SOCKET_TERMINATE_CHAR '\0'
 
-const char* MySocket::endMessage = ":end";
+// ked 1 strana posle 2. strane spravu ":end", tak chce ukoncit komunikaciu
+// specializovana sprava
+const char* MySocket::endMessage = ":end";  // upravit podla potreby
 
 MySocket* MySocket::createConnection(std::string hostName, short port) {
     WSADATA wsaData;
-    struct addrinfo* result = NULL;
+    struct addrinfo *result = NULL;
     struct addrinfo hints;
     int iResult;
 
+    // DLL -> pripojim sa pocas behu programu
+
     // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    // pri praci so socketmi vo Windowse musim explicitne zavolat WSAStartup
+    // "inicializujem" Windows kniznicu pre pracu so socketmi
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);  // STARTUP
     if (iResult != 0) {
         throw std::runtime_error("WSAStartup failed with error: " + std::to_string(iResult) + "\n");
     }
@@ -23,20 +31,27 @@ MySocket* MySocket::createConnection(std::string hostName, short port) {
     hints.ai_protocol = IPPROTO_TCP;
 
     // Resolve the server address and port
+    // === GETADDRINFO ===
+    // informacie o serveri, na ktory sa chcem pripojit
     iResult = getaddrinfo(hostName.c_str(), std::to_string(port).c_str(), &hints, &result);
     if (iResult != 0) {
-        WSACleanup();
+        // pre kazde volanie WSAStartup musim zavolat WSACleanup
+        WSACleanup();   // CLEANUP
         throw std::runtime_error("getaddrinfo failed with error: " + std::to_string(iResult) + "\n");
     }
 
     // Create a SOCKET for connecting to server
+    // === SOCKET ===
+    // vytvaram socket
     SOCKET connectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (connectSocket == INVALID_SOCKET) {
+    if (connectSocket == INVALID_SOCKET) {  // ak funkcia socket vrati nevalidnu hodnotu (socket nebol uspesne vytvoreny)
         WSACleanup();
         throw std::runtime_error("socket failed with error: " + std::to_string(WSAGetLastError()) + "\n");
     }
 
     // Connect to server
+    // === CONNECT ===
+    // pripojenie na server pomocou vytvoreneho socketu
     iResult = connect(connectSocket, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR) {
         closesocket(connectSocket);
@@ -50,35 +65,39 @@ MySocket* MySocket::createConnection(std::string hostName, short port) {
         WSACleanup();
         throw std::runtime_error("Unable to connect to server.\n");
     }
-    else
-    {
-        std::cout << "Successfully connected to server " << hostName << "\n";
-    }
 
+    // vytvorim objekt typu MySocket
+    // param: vytvoreny a pripojeny socket (zabaleny do MySocket)
     return new MySocket(connectSocket);
 }
 
 MySocket::MySocket(SOCKET socket) :
-    connectSocket(socket) 
-{
-
+        connectSocket(socket) {
+    // ziskavanie dat zo strany servera
 }
 
 MySocket::~MySocket() {
     if (this->connectSocket != INVALID_SOCKET) {
+        // uzavriem socket
         closesocket(this->connectSocket);
         this->connectSocket = INVALID_SOCKET;
     }
-    WSACleanup();
+    WSACleanup();   // vycisti po sebe
 }
 
-void MySocket::sendData(const std::string& data) {
+// ked klient len posiela data (retazec)
+void MySocket::sendData(const std::string &data) {
+    // uprava retazca, ktory chcem poslat cez socket
+    // pretoze nemusi platit, ze jeden write = jeden read
     size_t data_length = data.length();
     char* buffer = (char*)calloc(data_length + 1, sizeof(char));
     memcpy(buffer, data.c_str(), data_length);
+    // za kazdou odoslanou spravou poslem ukoncovaci znak
     buffer[data_length] = SOCKET_TERMINATE_CHAR;
 
-    int iResult = send(connectSocket, buffer, data_length + 1, 0);
+    // !! SEND !!
+    // poslem na socket svoj buffer, ktory ma data_length + 1 bajtov
+    int iResult = send(connectSocket, buffer, data_length + 1, 0 );
     if (iResult == SOCKET_ERROR) {
         throw std::runtime_error("send failed with error: " + std::to_string(WSAGetLastError()) + "\n");
     }
@@ -86,8 +105,14 @@ void MySocket::sendData(const std::string& data) {
     buffer = NULL;
 }
 
+// potom dorobit, aby klient mohol aj prijimat data
+
 void MySocket::sendEndMessage() {
+    // poslat ukoncovaciu spravu!
+    // chcem uplne ukoncit komunikaciu, uz som poslala vsetky spravy (data), ktore som chcela poslat
     this->sendData(this->endMessage);
 }
+
+// sprava moze prist rozkuskovana na viacero sprav (dokopy ten isty obsah), resp viac sprav moze prist ako jedna velka => pocet sprav NIE je garantovany
 
 #undef SOCKET_TERMINATE_CHAR
--- Client/MySocket.h
+++ Client/MySocket.h
@@ -1,22 +1,29 @@
-#pragma once
+#ifndef SOCKETS_CLIENT_MY_SOCKET_H
+#define SOCKETS_CLIENT_MY_SOCKET_H
 #pragma comment(lib, "ws2_32.lib")
-
 #include <winsock2.h>
-#include <stdexcept>
-#include <ws2tcpip.h>
 #include <string>
 
 class MySocket {
 public:
+    // umozni nadviazat spojenie so serverom
+    // ako param poslem meno serveru (dns zaznam) a cislo portu
+    // vytvori sa socket
+    // vykona sa connect
+    // vrati objekt MySocket -> nad nim uz mozem posielat data
     static MySocket* createConnection(std::string hostName, short port);
-
     ~MySocket();
+    // posielam retazec dat (vzdy po sieti posielam len retazec!!)
+    // posielam text => nemusim prevadzat na little/big endian
     void sendData(const std::string& data);
     void sendEndMessage();
 protected:
     MySocket(SOCKET socket);
 private:
-    static const char* endMessage;
+    static const char * endMessage;
+    // socket, pomocou ktoreho budem komunikovat so serverom
+    // alias pre typ unsigned int pointer
     SOCKET connectSocket;
 };
 
+#endif //SOCKETS_CLIENT_MY_SOCKET_H
