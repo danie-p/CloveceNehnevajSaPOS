c68b26baa1b7669922ce5f16f67bc71ecd1040f2
Kristián Lepka <kristianl12@proton.me>
2024-01-03 20:54:08 +0100

    work on game loop

--- Server/Game.cpp
+++ Server/Game.cpp
@@ -5,20 +5,74 @@
 #include "Game.h"
 
 namespace Server {
+
     Game::Game(std::vector<int> clientSockets) {
         this->playerSockets = clientSockets;
+        playerCount = clientSockets.size();
     }
 
-    void Game::Begin() {
-        for (int i = 0; i < playerSockets.size(); ++i) {
-            playerThreads.push_back(new std::thread());
+    Game::~Game() {
+        for (int i = 0; i < playerCount; ++i) {
+            if (playerThreads[i] != nullptr) {
+                playerThreads[i]->join();
+                delete playerThreads[i];
+                playerThreads[i] = nullptr;
+            }
+        }
+
+        if (boardUpdateThread != nullptr) {
+            boardUpdateThread->join();
+            delete boardUpdateThread;
+            boardUpdateThread = nullptr;
         }
     }
 
+    void Game::Begin() {
+        for (int i = 0; i < playerCount; ++i) {
+            playerThreads.push_back(new std::thread(&Game::ManagePlayerTurns, this, playerSockets[i]));
+        }
+
+        boardUpdateThread = new std::thread(&Game::UpdateBoard, this);
+    }
+
     void Game::End() {
-        for (int i = 0; i < playerSockets.size(); ++i) {
+        for (int i = 0; i < playerCount; ++i) {
             playerThreads[i]->join();
             delete playerThreads[i];
+            playerThreads[i] = nullptr;
+        }
+
+        boardUpdateThread->join();
+        delete boardUpdateThread;
+        boardUpdateThread = nullptr;
+    }
+
+    void Game::ManagePlayerTurns(int socket) {
+
+    }
+
+    void Game::UpdateBoard() {
+        // after applying the move of player on the board = after turn, send the board to players
+        while (true) {
+            {
+                // first wait until the turn is made
+                std::unique_lock<std::mutex> lock(mutex);
+                while (!turnDone)
+                    cvUpdateBoard.wait(lock);
+
+                for (int i = 0; i < playerCount; ++i) {
+                    if (/* not game over */1 == 2) {
+                        //std::string boardStr = board.toString();
+                        //write(playerSockets[i], boardStr.c_str(), boardStr.size() + 1);
+                    } else {
+                        // send results and return
+                        return;
+                    }
+                }
+
+                turnDone = false;
+                turn++;
+            }
         }
     }
 }
--- Server/Game.h
+++ Server/Game.h
@@ -7,24 +7,31 @@
 
 #include <vector>
 #include <thread>
-
+#include <unistd.h>
+#include <condition_variable>
 #include "Board.h"
 
 namespace Server {
-
-    void ManagePlayerTurns(int socket);
-
     class Game {
     private:
         Board board;
+        int turn = 1;
         int playerOnTurn = 0;
+        int playerCount = 0;
         std::vector<int> playerSockets;
         std::vector<std::thread*> playerThreads;
+        std::thread* boardUpdateThread;
+        std::condition_variable cvUpdateBoard;
+        std::mutex mutex;
+        bool turnDone = true;
 
     public:
         Game(std::vector<int> clientSockets);
+        ~Game();
         void Begin();
         void End();
+        void ManagePlayerTurns(int socket);
+        void UpdateBoard();
     };
 
 }
